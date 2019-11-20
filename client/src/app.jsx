a3d367792b3dae74b570b218b93feccaefe20575
terryjim <terryjim@163.com>
2019-11-20 17:47:41 +0800

    init

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,8 +1,8 @@
 import Taro, { Component } from '@tarojs/taro'
 import Index from './pages/index'
-
-import './app.scss'
-
+/*
+import './app.scss'*/
+import './styles/base.scss'
 // 如果需要在 h5 环境中开启 React Devtools
 // 取消以下注释：
 // if (process.env.NODE_ENV !== 'production' && process.env.TARO_ENV === 'h5')  {
