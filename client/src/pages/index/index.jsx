323b5821e3fbec14109e8a41ba719c534b136623
terryjim <terryjim@163.com>
2019-12-15 15:16:56 +0800

    课时购买界面

--- client/src/pages/index/index.jsx
+++ client/src/pages/index/index.jsx
@@ -1,6 +1,6 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, ScrollView, Image, Text, Swiper, SwiperItem, Map, Button } from '@tarojs/components'
-import { AtGrid, AtIcon, AtButton } from 'taro-ui'
+import { AtGrid, AtIcon, AtButton, AtCard } from 'taro-ui'
 import './index.scss'
 
 import Login from '../../components/login/index'
@@ -63,15 +63,15 @@ export default class Index extends Component {
         /* data: { action: 'query', needOpenid: true }*/
       })
       .then(res => {
-       /* Taro.showToast({
-            title: JSON.stringify(res),
-            icon: 'none',
-            duration: 5000
-          })
-          console.log(res)
-     if(2>1)
-     return*/
-     console.log(res)
+        /* Taro.showToast({
+             title: JSON.stringify(res),
+             icon: 'none',
+             duration: 5000
+           })
+           console.log(res)
+      if(2>1)
+      return*/
+        console.log(res)
         if (res.result.retCode && res.result.retCode !== 1)
           Taro.showToast({
             title: '您已领取过免费券，请直接使用！',
@@ -86,11 +86,10 @@ export default class Index extends Component {
           })
         }
         /* .then(res => console.log(res)) */
-      }).catch(e=>
-      {
+      }).catch(e => {
         console.log('----------------------------------------')
-      console.log(e)
-    })
+        console.log(e)
+      })
 
 
   }
@@ -100,7 +99,7 @@ export default class Index extends Component {
       <View className='mainView'>
         {/* <Login/>*/}
         <Swiper
-          className='defaultView swiper'
+          className='swiper'
           indicatorColor='#999'
           indicatorActiveColor='#333'
           /*  vertical */
@@ -137,25 +136,63 @@ export default class Index extends Component {
             <Button style={{ backgroundColor: '#fa8c16', color: '#ffffff' }} circle={true} size='mini'>立即领取</Button>
           </View>
         </View>
-        {/* <View style={{ marginTop: '10px', marginLeft: '10px' }}>
-          自习室地址：</View>*/}
-        <View className='defaultView at-row at-row__align--center at-row__justify--center' style={{ height: '50px' }}>
-          <View onClick={this.onTap} className='at-col  at-col-1 ' > <AtIcon value='map-pin' color='#fa8c16' /></View>
-          <View onClick={this.onTap} className='at-col  at-col-9 at-col--wrap'>
-            武汉市洪山区鲁磨路243号国光大厦B座1401室
+        快速购买
+        <View className='defaultView at-row at-row--wrap' >
+          <View className='at-col  at-col-6 mainView'/*  style={{textAlign: 'center' }} */>
+            <View style='margin: 5px;height: 75px;background:#389e0d;color:#fff' onClick={this.showWebView.bind(this, b.Url)}>
+              <View ><Text>课时卡</Text></View>
+              <View><Text>可自由转换为其它卡</Text></View>
+              <View> <Text>大惊喜！存就送</Text></View>
             </View>
-          <View className='at-col  at-col-2' >
-            <AtIcon value='phone' size='30' color='#fa8c16' onClick={this.onCall} /></View>
+          </View>
+          <View className='at-col  at-col-6 mainView' >
+            <View style='margin: 5px;height: 75px;background:#08979c;color:#fff' onClick={this.showWebView.bind(this, b.Url)}>
+              <View><Text>单日四小时卡</Text></View>
+              <View><Text>10 20元</Text></View>
+            </View>
+          </View>
+          <View className='at-col  at-col-6 mainView' style={{ textAlign: 'center' }}>
+            <View style='margin: 5px;height: 75px;background:#096dd9;color:#fff' onClick={this.showWebView.bind(this, b.Url)}>
+              <View ><Text>全日卡</Text></View>
+            </View>         
         </View>
-        <Map style={{ marginTop: '5px', height: '180px', width: '750px' }} latitude={30.512130} longitude={114.399730} markers={[{ latitude: 30.512130, longitude: 114.399730 }]} onClick={this.onTap} />
+        <View className='at-col  at-col-6 mainView' style={{ textAlign: 'center' }}>
+          <View style='margin: 5px;height: 75px;background:#1d39c4;color:#fff' onClick={this.showWebView.bind(this, b.Url)}>
+            <View ><Text>7日卡</Text></View>
+          </View>
+        </View>
+        <View className='at-col  at-col-6 mainView' style={{ textAlign: 'center' }}>
+          <View style='margin: 5px;height: 75px;background:#531dab;color:#fff' onClick={this.showWebView.bind(this, b.Url)}>
+
+            <View ><Text>30日卡</Text></View>
+          </View>
+        </View>
+        <View className='at-col  at-col-6 mainView' style={{ textAlign: 'center' }}>
+          <View style='margin: 5px;height: 75px;background:#c41d7f;color:#fff' onClick={this.showWebView.bind(this, b.Url)}>
+
+            <View ><Text>90日卡</Text></View>
+          </View>
+        </View>
       </View>
-    )
+          {/* <View style={{ marginTop: '10px', marginLeft: '10px' }}>
+          自习室地址：</View>*/}
+    <View className='defaultView at-row at-row__align--center at-row__justify--center' style={{ height: '50px' }}>
+      <View onClick={this.onTap} className='at-col  at-col-1 ' > <AtIcon value='map-pin' color='#fa8c16' /></View>
+      <View onClick={this.onTap} className='at-col  at-col-9 at-col--wrap'>
+        武汉市洪山区鲁磨路243号国光大厦B座1401室
+            </View>
+      <View className='at-col  at-col-2' >
+        <AtIcon value='phone' size='30' color='#fa8c16' onClick={this.onCall} /></View>
+    </View>
+    {/*   <Map style={{ marginTop: '5px', height: '180px', width: '750px' }} latitude={30.512130} longitude={114.399730} markers={[{ latitude: 30.512130, longitude: 114.399730 }]} onClick={this.onTap} /> */ }
+        </View >
+        )
   }
 }
 
 
 
-///////////////////////
+    ///////////////////////
 /*4.jpg	cloud://zxs5188.7a78-zxs5188-1300852908/banners/4.jpg	98.70 KB	刚刚
 3.jpg	cloud://zxs5188.7a78-zxs5188-1300852908/banners/3.jpg	52.31 KB	刚刚
 2.jpg	cloud://zxs5188.7a78-zxs5188-1300852908/banners/2.jpg	90.32 KB	刚刚
