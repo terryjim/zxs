28fefa69e5b877d7cbad3a04aa24bf4ec48d6007
terryjim <terryjim@163.com>
2019-12-16 16:57:54 +0800

    支付

--- cloud/functions/charge/index.js
+++ cloud/functions/charge/index.js
@@ -1,3 +1,11 @@
+const config={
+  appid: 'wx8f77fa93cb141527', //小程序Appid
+      envName: 'zf-shcud', // 小程序云开发环境ID
+      mchid: '1568694471', //商户号
+      partnerKey: '4f6il6blht67hl5C7GIPsHXFyRIN4BNJ', //此处填商户密钥
+      notify_url: 'https://mp.weixin.qq.com', //支付回调网址,这里可以随意填一个网址
+      spbill_create_ip: '127.0.0.1'
+}
 const cloud = require('wx-server-sdk')
 
 cloud.init()
